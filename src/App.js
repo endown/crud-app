52db23375fdeacc73886a170ca4696e72cdd0414
遠藤隼人 <endouhayato@endouhayatonoMacBook-Pro.local>
2021-03-08 00:24:54 +0900

    use React.Fragment

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+import React, { Component } from  'react';
 function App() {
   return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
+    <React.Fragment>
+      <input type="text" onClick={()=> {console.log("clicked!")}} />
+      <input type="text" onChange={()=> {console.log("clicked!")}} />
+    </React.Fragment>
   );
 }
 
